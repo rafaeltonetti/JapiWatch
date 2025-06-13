eac586387919994b2f705edc35b93cd0a7c41d75
rafael <rafaeltonetti.cardoso@gmail.com>
2025-06-13 15:11:40 -0300

    Versão mais atualizada

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,23 @@
 <?php
+
 session_start();
-if (!isset($_SESSION['usuario_id'])) {
-    header("Location: login.php");
-    exit();
+
+include 'conexao.php';
+
+if (!isset($_SESSION["usuario_nome"])){
+    header("location: login.php");
 }
+
 ?>
 
-<h1>Bem-vindo, <?php echo $_SESSION['usuario_nome']; ?>!</h1>
-<a href="logout.php">Sair</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $_SESSION["usuario_nome"] ?></title>
+</head>
+<body>
+
+</body>
+</html>
