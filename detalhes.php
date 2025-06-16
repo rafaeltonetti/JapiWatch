f1416752f790de156a63d83722d2265bb138ce57
MateusCarnauba <mateuscarnauba1@gmail.com>
2025-06-16 01:07:44 -0300

    Update detalhes.php
    habilitei o link da logo

--- detalhes.php
+++ detalhes.php
@@ -140,7 +140,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['comentario'])) {
 <body>
     <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
         <div class="container-fluid gap-3">
-            <a class="navbar-brand" style="margin-left: 30px" href="#">
+            <a class="navbar-brand" style="margin-left: 30px" href="index.php">
                 <img src="img/logo.png" alt="Logo" width="120" height="55" class="d-inline-block align-text-top">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
